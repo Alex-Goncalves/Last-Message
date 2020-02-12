84794f23958ef5f878be0d5881118e64fb7146af
Alex-Goncalves <60947441+Alex-Goncalves@users.noreply.github.com>
2020-02-12 16:05:51 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function submitText() {
     let message = document.getElementById("message").value;
-    document.getElementById("lastMessage").innerHTML = `Last Message: ${message}`;
+    if (message !== "") {
+        document.getElementById('lastMessage').style.color = 'rgb(90, 120, 250)';
+        document.getElementById("lastMessage").innerHTML = `Last Message: ${message}`;
+        document.getElementById('message').innerHTML = 'NO';
+    }
+    else {
+        document.getElementById("lastMessage").style.color = 'red';
+        document.getElementById("lastMessage").innerHTML = `Message Invalid!`;
+    }
+}
+
+function bold() {
+    let message = document.getElementById("lastMessage");
+    message.innerHTML = `<i>${message.value}</i>`;
 }
