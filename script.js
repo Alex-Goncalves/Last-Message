2c8e46af1e8c39d27d0ac5c80426e1009cabeea0
Alex-Goncalves <60947441+Alex-Goncalves@users.noreply.github.com>
2020-02-13 19:11:56 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 function submitText() {
     let message = document.getElementById("message").value;
     if (message !== "") {
-        document.getElementById('lastMessage').style.color = 'rgb(90, 120, 250)';
-        document.getElementById("lastMessage").innerHTML = `Last Message: ${message}`;
-        document.getElementById('message').innerHTML = 'NO';
+        document.getElementById("lastMessage").innerHTML = message;
+        document.getElementById('message').value = "";
     }
     else {
         document.getElementById("lastMessage").style.color = 'red';
@@ -11,7 +10,33 @@ function submitText() {
     }
 }
 
-function bold() {
-    let message = document.getElementById("lastMessage");
-    message.innerHTML = `<i>${message.value}</i>`;
+function italic() {
+    document.getElementById('lastMessage').style.fontStyle = "italic";
+}
+
+function color() {
+    let c1 = (Math.floor(Math.random() * 251)).toString();
+    let c2 = (Math.floor(Math.random() * 251)).toString();
+    let c3 = (Math.floor(Math.random() * 251)).toString();
+    document.getElementById('lastMessage').style.color = `rgb(${c1}, ${c2}, ${c3})`;
+}
+
+function bigger() {
+    document.getElementById('lastMessage').style.fontSize = '30px';
+}
+
+function smaller() {
+    document.getElementById("lastMessage").style.fontSize = '17px';
+}
+
+function initial() {
+    document.getElementById('lastMessage').style.fontSize = '24px';
+    document.getElementById('lastMessage').style.fontStyle = "normal";
+    document.getElementById('lastMessage').style.color = `rgb(90, 120, 250)`;
+}
+
+function test() {
+    let size = window.getComputedStyle(document.getElementById("lastMessage")).fontSize;
+    let fontSize = parseInt(size);
+    document.getElementById('lastMessage').style.fontSize = `${fontSize + 2}px`;
 }
