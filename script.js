2e7b31c697f277f2933ca2fd1865da841369c852
Alex-Goncalves <60947441+Alex-Goncalves@users.noreply.github.com>
2020-02-15 20:00:52 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,59 @@
 function submitText() {
     let message = document.getElementById("message").value;
     if (message !== "") {
+        document.getElementById('lastMessage').style.fontSize = '24px';
+        document.getElementById('lastMessage').style.fontStyle = "normal";
+        document.getElementById('lastMessage').style.color = `rgb(90, 120, 250)`;
         document.getElementById("lastMessage").innerHTML = message;
         document.getElementById('message').value = "";
     }
     else {
         document.getElementById("lastMessage").style.color = 'red';
-        document.getElementById("lastMessage").innerHTML = `Message Invalid!`;
+        document.getElementById("lastMessage").innerHTML = `Invalid Message!`;
     }
 }
 
 function italic() {
-    document.getElementById('lastMessage').style.fontStyle = "italic";
-}
-
-function color() {
-    let c1 = (Math.floor(Math.random() * 251)).toString();
-    let c2 = (Math.floor(Math.random() * 251)).toString();
-    let c3 = (Math.floor(Math.random() * 251)).toString();
-    document.getElementById('lastMessage').style.color = `rgb(${c1}, ${c2}, ${c3})`;
+    if (document.getElementById('lastMessage').innerHTML !== 'Invalid Message!') {
+        document.getElementById('lastMessage').style.fontStyle = "italic";
+    }
 }
 
 function bigger() {
-    document.getElementById('lastMessage').style.fontSize = '30px';
+    if (document.getElementById('lastMessage').innerHTML !== 'Invalid Message!') {
+        let size = window.getComputedStyle(document.getElementById("lastMessage")).fontSize;
+        let fontSize = parseInt(size);
+        if (fontSize < 40) {
+            document.getElementById('lastMessage').style.fontSize = `${fontSize + 2}px`;
+        }
+    }
 }
 
 function smaller() {
-    document.getElementById("lastMessage").style.fontSize = '17px';
+    if (document.getElementById('lastMessage').innerHTML !== 'Invalid Message!') {
+        let size = window.getComputedStyle(document.getElementById("lastMessage")).fontSize;
+        let fontSize = parseInt(size);
+        if (fontSize > 12) {
+            document.getElementById('lastMessage').style.fontSize = `${fontSize - 2}px`;
+        }
+    }
 }
+
+function color() {
+    if (document.getElementById('lastMessage').innerHTML !== 'Invalid Message!') {
+        let c1 = (Math.floor(Math.random() * 251)).toString();
+        let c2 = (Math.floor(Math.random() * 251)).toString();
+        let c3 = (Math.floor(Math.random() * 251)).toString();
+        document.getElementById('lastMessage').style.color = `rgb(${c1}, ${c2}, ${c3})`;
+    }
+}
+
 
 function initial() {
-    document.getElementById('lastMessage').style.fontSize = '24px';
-    document.getElementById('lastMessage').style.fontStyle = "normal";
-    document.getElementById('lastMessage').style.color = `rgb(90, 120, 250)`;
+    if (document.getElementById('lastMessage').innerHTML !== 'Invalid Message!') {
+        document.getElementById('lastMessage').style.fontSize = '24px';
+        document.getElementById('lastMessage').style.fontStyle = "normal";
+        document.getElementById('lastMessage').style.color = `rgb(90, 120, 250)`;
+    }
 }
 
-function test() {
-    let size = window.getComputedStyle(document.getElementById("lastMessage")).fontSize;
-    let fontSize = parseInt(size);
-    document.getElementById('lastMessage').style.fontSize = `${fontSize + 2}px`;
-}
